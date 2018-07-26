970dcbee91d7567decf5ad6e7e665f5b2e48a6e8
Cem Bozsahin <cem.bozsahin@gmail.com>
2018-07-26 21:55:50 +0300

    logs ccglab version as well in nohup

--- bin/train-nohup-sbcl.lisp
+++ bin/train-nohup-sbcl.lisp
@@ -7,6 +7,6 @@
     (sb-ext:gc) ; make effective immediately
     (time (update-model gram N alpha0 c :load t)) ; cross your fingers
     (show-training)                     ; prayers answered
-    (which-ccglab)                      ; for the record
+    (pprint (which-ccglab))             ; for the record
     (and savep (save-training out))     ; session output always to nohup.out when called by ccglab.nohup
     ))
