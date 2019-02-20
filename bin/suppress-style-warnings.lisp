1602c25d0abbfa99f3e550a845f5b79dc80913e3
bozsahin <cem.bozsahin@gmail.com>
2019-02-20 12:41:58 +0300

    muffled style warnings of sbcl and some in ccl in 5.2

--- bin/suppress-style-warnings.lisp
+++ bin/suppress-style-warnings.lisp
@@ -1,5 +1,8 @@
 ;; to suppress style warnings --load before everything else
-;; this is sbcl-specific, but because of declaim it is ignored by others rather than give error
+;; first form is SBCL-specific, but because of declaim it is ignored by others rather than give error.
+;; CCL isn't very chatty anyway.
 ;; -cem bozsahin
 
 (declaim #+sbcl(sb-ext:muffle-conditions style-warning))
+(setf *load-verbose* nil)
+(setf *load-print* nil)
