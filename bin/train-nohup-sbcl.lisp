4f0bf1ceecfe3dae2ee42bb6b88f185dbe47cc32
bozsahin <cem.bozsahin@gmail.com>
2019-03-06 09:46:20 +0300

    style fixes in train-nohup-sbcl

--- bin/train-nohup-sbcl.lisp
+++ bin/train-nohup-sbcl.lisp
@@ -5,9 +5,9 @@
     (setf (sb-ext:bytes-consed-between-gcs) (* gcmb 1024 1024))  ; specify GC cycle in MB
     ; default is 51 MB in sbcl
     (sb-ext:gc) ; make effective immediately
-    (pprint "----------------")
+    (format t "~%----------------")
     (pprint (which-ccglab))             ; for the record
-    (pprint "----------------")
+    (format t "~%----------------")
     (status)                            ; ditto  (summarises rule use etc)
     (time (update-model gram N alpha0 c :load t)) ; cross your fingers
     (show-training)                     ; prayers answered
