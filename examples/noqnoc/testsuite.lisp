cefb2e20281c397f48ffdaa1ef148ff22a74f46f
bozsahin <cem.bozsahin@gmail.com>
2019-06-09 15:07:33 +0300

    revised noqnoc

--- examples/noqnoc/testsuite.lisp
+++ examples/noqnoc/testsuite.lisp
@@ -9,3 +9,13 @@
     (um "g1" 10 1.0 1.0 :load t)  ; updates g1.ind after loading it, using g1.sup
     (st)			; shows training
     ))
+
+(defun test-noqnoc-xp ()
+  "in case we want to extrapolae the gradient"
+  (progn
+    (lg "g1")               ; loads the .ded file 
+    (sg "g1.ind")           ; saves currently loaded grammar as .ind-- for modeling
+    (nf-parse-on)           ; use normal form parsing
+    (umxp "g1" 1.0 1.0 :load t)  ; updates g1.ind after loading it, using g1.sup
+    (show-training-xp)			; shows training
+    ))
